b9163d4d9c225351100fce9e34703ea65e557b5a
Jan Sepke <625043+jansepke@users.noreply.github.com>
2020-11-10 14:22:35 +0100

    npm update

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-// typescript definition currently missing in package
-// https://github.com/vinissimus/next-translate/issues/88
-declare module "next-translate/useTranslation" {
-  export default function useTranslation(): {
-    t: (key: string, query?: { [name: string]: string | number }) => string;
-    lang: string;
-  };
-}
