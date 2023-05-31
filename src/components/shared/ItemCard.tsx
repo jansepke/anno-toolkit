8a50814b5f1630fedacc4eeddfcdf47c8efe905e
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-05-31 22:28:43 +0200

    refactor ItemCard

--- src/components/shared/ItemCard.tsx
+++ src/components/shared/ItemCard.tsx
@@ -3,6 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
 import Image from "next/image";
 import React from "react";
 import { rarities } from "../../anno-config";
@@ -10,28 +11,29 @@ import { AnnoItem } from "../../data/AnnoItem";
 
 const raritiesByKey = rarities.reduce((all: Record<string, string>, r) => ({ ...all, [r.key]: r.color }), {});
 
-// TODO: use styled components
-const ItemCard = ({
-  item,
-  titleSuffix,
-  children,
-}: {
+const CustomCard = styled(Card)({
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  borderWidth: 2,
+  borderStyle: "solid",
+});
+
+const CustomCardContent = styled(CardContent)({
+  maxHeight: "12rem",
+  overflow: "auto",
+  paddingTop: "0",
+  marginBottom: "auto",
+});
+
+interface ItemCardProps extends React.PropsWithChildren {
   item: AnnoItem;
   titleSuffix?: React.ReactNode;
-  children: React.ReactNode;
-}) => (
+}
+
+const ItemCard: React.FC<ItemCardProps> = ({ item, titleSuffix, children }) => (
   <Grid item xs={12} sm={6} md={4} lg={3} xl={2} sx={{ display: "flex" }}>
-    <Card
-      elevation={3}
-      sx={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        borderWidth: 2,
-        borderColor: raritiesByKey[item.rarity],
-        borderStyle: "solid",
-      }}
-    >
+    <CustomCard elevation={3} sx={{ borderColor: raritiesByKey[item.rarity] }}>
       <CardHeader
         avatar={<Image src={item.icon} width={35} height={35} unoptimized alt="" />}
         title={
@@ -56,17 +58,8 @@ const ItemCard = ({
           </>
         }
       />
-      <CardContent
-        sx={{
-          maxHeight: "12rem",
-          overflow: "auto",
-          paddingTop: "0",
-          marginBottom: "auto",
-        }}
-      >
-        {children}
-      </CardContent>
-    </Card>
+      <CustomCardContent>{children}</CustomCardContent>
+    </CustomCard>
   </Grid>
 );
 
