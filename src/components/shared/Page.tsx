04af50dc7c6d6eb4740d8ef015b8244f8ee04f31
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-05-31 22:04:26 +0200

    update title

--- src/components/shared/Page.tsx
+++ src/components/shared/Page.tsx
@@ -2,18 +2,20 @@ import Head from "next/head";
 import React from "react";
 import TopBar from "./TopBar";
 
-const Page = ({ headline, children }: { headline: string; children: React.ReactNode }) => {
-  return (
-    <>
-      <Head>
-        <title>Anno toolkit</title>
-        <meta name="description" content="Finding the best fitting specialists." />
-      </Head>
-      <TopBar headline={headline} />
+interface PageProps extends React.PropsWithChildren {
+  headline: string;
+}
 
-      {children}
-    </>
-  );
-};
+const Page: React.FC<PageProps> = ({ headline, children }) => (
+  <>
+    <Head>
+      <title>{headline}</title>
+      <meta name="description" content="Finding the best fitting specialists." />
+    </Head>
+    <TopBar headline={headline} />
+
+    {children}
+  </>
+);
 
 export default Page;
